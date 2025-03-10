56c4768a051ff8b03f815044e19a20755abdcbdd
unknown <TT442@ccs.local>
2025-03-10 17:16:25 +0800

    Question 2

--- question2.js
+++ question2.js
@@ -1,3 +1,5 @@
+//Write a JavaScript function named reverseString that takes a string as an argument and returns the reversed string
+
 function reverse(s) {
     return s.split("").reverse().join("");
 }
